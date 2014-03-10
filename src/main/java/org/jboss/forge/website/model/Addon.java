cd5eb1ff2ac84c06c8cbe405034c63b3dbabfc00
George Gastaldi <gegastaldi@gmail.com>
2014-03-10 17:32:00 -0300

    Added REST services for Addon entity and changed persistence.xml

--- src/main/java/org/jboss/forge/website/model/Addon.java
+++ src/main/java/org/jboss/forge/website/model/Addon.java
@@ -9,89 +9,108 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Version;
 import java.lang.Override;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
-public class Addon implements Serializable {
+@XmlRootElement
+public class Addon implements Serializable
+{
+   private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", updatable = false, nullable = false)
-	private Long id;
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "id", updatable = false, nullable = false)
+   private Long id;
 
-	@Version
-	@Column(name = "version")
-	private int version;
+   @Version
+   @Column(name = "version")
+   private int version;
 
-	@Column
-	private String groupId;
+   @Column
+   private String groupId;
 
-	@Column
-	private String artifactId;
+   @Column
+   private String artifactId;
 
-	public Long getId() {
-		return this.id;
-	}
+   public Long getId()
+   {
+      return this.id;
+   }
 
-	public void setId(final Long id) {
-		this.id = id;
-	}
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
 
-	public int getVersion() {
-		return this.version;
-	}
+   public int getVersion()
+   {
+      return this.version;
+   }
 
-	public void setVersion(final int version) {
-		this.version = version;
-	}
+   public void setVersion(final int version)
+   {
+      this.version = version;
+   }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Addon)) {
-			return false;
-		}
-		Addon other = (Addon) obj;
-		if (id != null) {
-			if (!id.equals(other.id)) {
-				return false;
-			}
-		}
-		return true;
-	}
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (this == obj)
+      {
+         return true;
+      }
+      if (!(obj instanceof Addon))
+      {
+         return false;
+      }
+      Addon other = (Addon) obj;
+      if (id != null)
+      {
+         if (!id.equals(other.id))
+         {
+            return false;
+         }
+      }
+      return true;
+   }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+   @Override
+   public int hashCode()
+   {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
 
-	public String getGroupId() {
-		return this.groupId;
-	}
+   public String getGroupId()
+   {
+      return this.groupId;
+   }
 
-	public void setGroupId(final String groupId) {
-		this.groupId = groupId;
-	}
+   public void setGroupId(final String groupId)
+   {
+      this.groupId = groupId;
+   }
 
-	public String getArtifactId() {
-		return this.artifactId;
-	}
+   public String getArtifactId()
+   {
+      return this.artifactId;
+   }
 
-	public void setArtifactId(final String artifactId) {
-		this.artifactId = artifactId;
-	}
+   public void setArtifactId(final String artifactId)
+   {
+      this.artifactId = artifactId;
+   }
 
-	@Override
-	public String toString() {
-		String result = getClass().getSimpleName() + " ";
-		if (groupId != null && !groupId.trim().isEmpty())
-			result += "groupId: " + groupId;
-		if (artifactId != null && !artifactId.trim().isEmpty())
-			result += ", artifactId: " + artifactId;
-		return result;
-	}
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (groupId != null && !groupId.trim().isEmpty())
+         result += "groupId: " + groupId;
+      if (artifactId != null && !artifactId.trim().isEmpty())
+         result += ", artifactId: " + artifactId;
+      return result;
+   }
 }
