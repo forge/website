43cb39a65e1b699ed03e535de2e8a12fdf0633d7
George Gastaldi <gegastaldi@gmail.com>
2014-03-26 19:54:01 -0300

    Removed REST resources for now

--- src/main/java/org/jboss/forge/website/rest/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.forge.website.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application
-{
-}
