8cbc479ff75fdee1a68ebaa2b9b3792d1263d927
George Gastaldi <gegastaldi@gmail.com>
2014-04-09 14:56:30 -0300

    Documentation section is now dynamic

--- src/main/java/org/jboss/forge/website/model/Document.java
+++ src/main/java/org/jboss/forge/website/model/Document.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -34,6 +36,15 @@ public class Document implements Serializable
 
    @Column
    private String path;
+
+   @Column
+   private String title;
+
+   @Column
+   private String description;
+
+   @Enumerated(EnumType.ORDINAL)
+   private DocumentCategory category;
 
    public Long getId()
    {
@@ -127,5 +138,35 @@ public class Document implements Serializable
       if (path != null && !path.trim().isEmpty())
          result += ", path: " + path;
       return result;
+   }
+
+   public String getTitle()
+   {
+      return title;
+   }
+
+   public void setTitle(String title)
+   {
+      this.title = title;
+   }
+
+   public String getDescription()
+   {
+      return description;
+   }
+
+   public void setDescription(String description)
+   {
+      this.description = description;
+   }
+
+   public DocumentCategory getCategory()
+   {
+      return category;
+   }
+
+   public void setCategory(DocumentCategory category)
+   {
+      this.category = category;
    }
 }
