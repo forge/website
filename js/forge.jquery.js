8338b2b13338d24dfcb4cc29d5591bc14741ce7a
visuale <james@visuale.net>
2015-01-24 23:16:56 -0600

    Updates to CSS and add new Addons page, with its own CSS file (for now – will probably be combined with main CSS). Star rating script is helped in part by the 'data-rating' attribute for each star-row class. Star symbols are included via a JQuery Script.

--- js/forge.jquery.js
+++ js/forge.jquery.js
@@ -14,7 +14,11 @@ $(function() {
     // Maintain position of emblem if viewing on a phone-size screen
     smallScreenWidthMgr();
 
+    if($('.star-row').length) {
+        uniformAddonSizes();
+        addStars();
 
+    }
 
 });
 
@@ -127,5 +131,56 @@ function alignGreyBoxes() {
 
         // Apply CSS
         $('.grey-div.center-div').css({'marginTop':'-'+newMargin+'px'});
+
+}
+
+
+// Function to set a standard size for all the addon box descriptions (and by extension all the addon divs) based on largest description.
+function uniformAddonSizes() {
+
+    var heightArr = new Array();
+
+    // Gather heights of all <p> elements
+    $('.addon').each(function(i,e) {
+       heightArr.push($(this).children('p').height());
+    });
+
+    // Determine the largest height
+    var allHeight = Math.max.apply(Math,heightArr);
+
+    // Set the height to all <p> tags
+    $('.addon').each(function(i,e) {
+        $(this).children('p').css({'height':allHeight+'px'});
+    });
+}
+
+
+
+// Function to determine how many stars an addon has been rated.
+// The function takes the data-rating value and changes that many stars for each row instance from grey to gold by adding the 'gold' class.
+function addStars() {
+    $('.star-row').each(function(i,e) {
+        // First, insert 5 star glyphicons, with their default settings
+        var basicStar = '<span class="glyphicon glyphicon-star"> </span>';
+
+        var rating = $(this).attr('data-rating');
+
+        // Insert 5 star glyphicon span tags
+        for(i=0;i<5;i++) {
+            if(i==0) {  $(this).html(basicStar);
+            } else
+            if(i>0) {   $(this).children('span:last-child').after(basicStar); }
+        }
+
+        // This takes the data-rating value and loops through the inserted star icons to change the given number to gold.
+        if(rating > 0) {
+            var starCount = parseInt(rating) + 1;
+            for(i=0;i<starCount;i++) {
+                $(this).children('span:nth-child('+i+')').addClass('gold');
+            }
+        }
+
+    });
+
 
 }
