3594dd8747e924b57fd8434f741286a9d7ff684c
George Gastaldi <gegastaldi@gmail.com>
2015-04-29 12:03:39 -0300

    Added 1.x content

--- server.js
+++ server.js
@@ -3,9 +3,15 @@
 var cc          = require('config-multipaas'),
     restify     = require('restify'),
     fs          = require('fs'),
-    yaml        = require('js-yaml')
+    yaml        = require('js-yaml'),
+    fetchUrl    = require("fetch").fetchUrl;
 
-var config      = cc(),
+var config      = cc()
+                    .add(
+                        {
+                            "FORGE_WEBSITE_DATA_URL": 'https://github.com/forge/website-data',
+                            "FORGE_WEBSITE_DATA_DIR": (process.env.OPENSHIFT_DATA_DIR || '/tmp')  + '/website-data'
+                        }),
     app         = restify.createServer()
 
 app.use(restify.queryParser())
@@ -21,11 +27,6 @@ var mkdirSync = function (path) {
   }
 }
 
-var websiteDataURL = 'https://github.com/forge/website-data';
-
-// The git directory
-var gitDir = (process.env.OPENSHIFT_DATA_DIR || '/tmp')  + '/website-data';
-
 // Caching index.html to minimize IO
 var indexHTML = fs.readFileSync(__dirname + '/app/index.html');
 
@@ -37,36 +38,38 @@ app.get('/', function (req, res, next)
   res.end(indexHTML);
 });
 
-app.get(/\/1.x\/?.*/, restify.serveStatic({directory: './1.x/'}));
-app.get(/\/(css|fonts|images|js|views)\/?.*/, restify.serveStatic({directory: './app/'}));
+app.get(/\/(1\.x|css|fonts|images|js|views)\/?.*/, restify.serveStatic({directory: './app/'}));
 
+app.get('/addons', function(req, res) {
+    var addons = 
+    { 
+        'community':
+        [
+        {
+            id: 'org.jboss.forge.addon:reflections',
+            name: 'Reflections', 
+            description: 'Enables the usage of the Reflections library as project facets for Java runtime metadata analysis', 
+            author:'George Gastaldi', 
+            rating: 5,
+        }
+        ], 
+        'core':
+        [
+        {
+            id: 'org.jboss.forge.addon:gradle',
+            name: 'Gradle', 
+            description: 'Enables Grade in your project', 
+            author:'Lincoln Baxter III', 
+            rating: 3,
+            logo: 'https://pbs.twimg.com/profile_images/2149314222/square.png',
+        }
+        ]
+    };    
+    res.json(addons);
+});
 
-var addons = 
-{ 
-    'community':
-    [
-    {
-        id: 'org.jboss.forge.addon:reflections',
-        name: 'Reflections', 
-        description: 'Enables the usage of the Reflections library as project facets for Java runtime metadata analysis', 
-        author:'George Gastaldi', 
-        rating: 5,
-    }
-    ], 
-    'core':
-    [
-    {
-        id: 'org.jboss.forge.addon:gradle',
-        name: 'Gradle', 
-        description: 'Enables Grade in your project', 
-        author:'Lincoln Baxter III', 
-        rating: 3,
-        logo: 'https://pbs.twimg.com/profile_images/2149314222/square.png',
-    }
-    ]
-};
-
-var docs = [
+app.get('/docs', function(req, res) {
+    var docs = [
     {
         level: 'beginner',
         group: 'core',
@@ -88,24 +91,18 @@ var docs = [
         title: 'Write a Java EE Web Application - Advanced',
         summary: 'Use Forge to create a entire Java EE Web application.'
     }
-];
-
-app.get('/addons', function(req, res) {
-    res.json(addons);
-});
-
-app.get('/docs', function(req, res) {
-  res.json(docs);
+    ];
+    res.json(docs);
 });
 
 app.get('/news', function(req, res) {
-    var body = fs.readFileSync(gitDir + "/docs-news.yaml");
+    var body = fs.readFileSync(config.get('FORGE_WEBSITE_DATA_DIR') + "/docs-news.yaml");
     var allEntries = yamlLoad(body);
     res.json(allEntries);
 });
 
 app.listen(config.get('PORT'), config.get('IP'), function () {
-  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') );
 });
 
 
@@ -120,8 +117,8 @@ function yamlLoad(body) {
 }
 
 function gitPullWebsiteData() { 
-    mkdirSync(gitDir);
-    
+    mkdirSync(config.get('FORGE_WEBSITE_DATA_DIR'));
+    console.log( "Created " + config.get('FORGE_WEBSITE_DATA_DIR') );
 }
 
 gitPullWebsiteData();
