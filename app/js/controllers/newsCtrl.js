7d5c4d52504f377e64f0c3bd0918207ecbeb8f1e
George Gastaldi <gegastaldi@gmail.com>
2015-05-07 16:38:46 -0300

    Advanced Search in News section is now functional. Layout needs reviewing

--- app/js/controllers/newsCtrl.js
+++ app/js/controllers/newsCtrl.js
@@ -9,6 +9,46 @@ angular.module('jboss-forge').controller('newsCtrl', function($scope, $statePara
 	} else { 
 		backendAPI.fetchNews(function(data) { 
 			$scope.news = data;
+			var tags = [];
+			data.forEach(function (item) {
+				item.tags.split(',').forEach(
+					function(tag) {
+						tags.push(tag.trim());
+					}
+				);
+			});
+			$scope.newsCategories = 			
+				tags.filter(function (item, pos, self) {
+					return self.indexOf(item) == pos;			
+				}
+			);
+			$scope.selectedCategories = [];
+			$scope.addCategory = function(item, selectedCategories) {
+		        var i = $.inArray(item, selectedCategories);
+		        if (i > -1) {
+		            selectedCategories.splice(i, 1);
+		        } else {
+		            selectedCategories.push(item);
+		        }
+			}
+
+			$scope.clearCategories = function() { 
+				$scope.selectedCategories = [];
+			}
+			$scope.categoryFilter = function(item) {
+		        if ($scope.selectedCategories.length > 0) {
+		        	var allTags = item.tags.split(',');
+		        	for (i=0;i<$scope.selectedCategories.length;i++) 
+		        	{
+						if (item.tags.contains($scope.selectedCategories[i])) { 
+							return item;
+						}
+		        	}
+		        	return;
+		        }
+				return item;
+			}
+
 		});
 	}
 });
