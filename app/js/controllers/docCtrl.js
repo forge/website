952981b1506665c24cb1b73659e9fe0beb3528f2
George Gastaldi <gegastaldi@gmail.com>
2015-05-28 17:30:04 -0300

    Documentation links are ALIVE

--- app/js/controllers/docCtrl.js
+++ app/js/controllers/docCtrl.js
@@ -1,7 +1,7 @@
 angular.module('jboss-forge').controller('docCtrl', function($scope, $stateParams, backendAPI){
 	if ($stateParams.docId) {
 		backendAPI.fetchDocById($stateParams.docId, function (selectedDoc) { 
-			$scope.selectedDoc = selectedDoc;
+			$scope.doc = selectedDoc;
 		});
 		backendAPI.fetchDocContents($stateParams.docId, function (_htmlContents) { 
 			$scope.docContents = _htmlContents;
@@ -12,45 +12,6 @@ angular.module('jboss-forge').controller('docCtrl', function($scope, $stateParam
 	} else { 
 		backendAPI.fetchDocs(function(data) { 
 			$scope.docs = data;
-			// var tags = [];
-			// data.forEach(function (item) {
-			// 	item.tags.split(',').forEach(
-			// 		function(tag) {
-			// 			tags.push(tag.trim());
-			// 		}
-			// 	);
-			// });
-			// $scope.docsCategories = 			
-			// 	tags.filter(function (item, pos, self) {
-			// 		return self.indexOf(item) == pos;			
-			// 	}
-			// );
-			// $scope.selectedCategories = [];
-			// $scope.addCategory = function(item, selectedCategories) {
-		 //        var i = $.inArray(item, selectedCategories);
-		 //        if (i > -1) {
-		 //            selectedCategories.splice(i, 1);
-		 //        } else {
-		 //            selectedCategories.push(item);
-		 //        }
-			// }
-			// $scope.clearCategories = function() { 
-			// 	$scope.selectedCategories = [];
-			// }
-			// $scope.categoryFilter = function(item) {
-		 //        if ($scope.selectedCategories.length > 0) {
-		 //        	var allTags = item.tags.split(',');
-		 //        	for (i=0;i<$scope.selectedCategories.length;i++) 
-		 //        	{
-			// 			if (item.tags.contains($scope.selectedCategories[i])) { 
-			// 				return item;
-			// 			}
-		 //        	}
-		 //        	return;
-		 //        }
-			// 	return item;
-			// }
-
 		});
 	}	
 });
