b5124d1e9555c2a7740c6116c6c6d2b1e9ad5cb5
George Gastaldi <gegastaldi@gmail.com>
2015-07-02 15:20:24 -0300

    Cleanup #2

--- app/js/controllers/addonCtrl.js
+++ app/js/controllers/addonCtrl.js
@@ -1,52 +1,11 @@
 angular.module('jboss-forge').controller('addonCtrl', function($scope, $stateParams, backendAPI){
-	backendAPI.fetchAddons(function(data) { 
-		$scope.addons = data;
-		var tags = [];
-		for (type in data) { 
-			data[type].forEach(function (item) {
-				if (item.tags) {
-					item.tags.forEach(
-						function(tag) {
-							tags.push(tag.trim());
-						}
-					);
-				}
+	if ($stateParams.addonId) { 
+		// Addon detail page
+		$scope.fetchDocContents = function(addonId, docId) { 
+			backendAPI.fetchAddonDocContentsById(addonId, docId, function (data) {		
+				$scope.addon.docContents = data;
 			});
 		}
-		$scope.addonTags = tags.filter(function (item, pos, self) {
-			return self.indexOf(item) == pos;			
-		}).sort();
-		$scope.selectedTags = [];
-		$scope.addTag = function(item, selectedTags) {
-			var i = $.inArray(item, selectedTags);
-			if (i > -1) {
-				selectedTags.splice(i, 1);
-			} else {
-				selectedTags.push(item);
-			}
-		}
-		$scope.clearTags = function() { 
-			$scope.selectedTags = [];
-		}
-		$scope.tagFilter = function(item) {
-			if ($scope.selectedTags.length > 0) {
-				for (i=0;i<$scope.selectedTags.length;i++) 
-				{
-					if (item.tags && item.tags.indexOf($scope.selectedTags[i]) > -1) { 
-						return item;
-					}
-				}
-				return;
-			}
-			return item;
-		}
-	});
-	$scope.fetchDocContents = function(addonId, docId) { 
-		backendAPI.fetchAddonDocContentsById(addonId, docId, function (data) {		
-			$scope.addon.docContents = data;
-		});
-	}
-	if ($stateParams.addonId) { 
 		backendAPI.fetchAddonById($stateParams.addonId, function(addon) { 
 			$scope.addon = addon;
 			backendAPI.fetchAddonDocsById(addon.id, function(docSections) {
@@ -56,6 +15,50 @@ angular.module('jboss-forge').controller('addonCtrl', function($scope, $statePar
 					$scope.addon.docContents = docContents;
 				});
 			});
+		});
+	} else { 
+		// Addon main page
+		backendAPI.fetchAddons(function(data) { 
+			$scope.addons = data;
+			var tags = [];
+			for (type in data) { 
+				data[type].forEach(function (item) {
+					if (item.tags) {
+						item.tags.forEach(
+							function(tag) {
+								tags.push(tag.trim());
+							}
+						);
+					}
+				});
+			}
+			$scope.addonTags = tags.filter(function (item, pos, self) {
+				return self.indexOf(item) == pos;			
+			}).sort();
+			$scope.selectedTags = [];
+			$scope.addTag = function(item, selectedTags) {
+				var i = $.inArray(item, selectedTags);
+				if (i > -1) {
+					selectedTags.splice(i, 1);
+				} else {
+					selectedTags.push(item);
+				}
+			}
+			$scope.clearTags = function() { 
+				$scope.selectedTags = [];
+			}
+			$scope.tagFilter = function(item) {
+				if ($scope.selectedTags.length > 0) {
+					for (i=0;i<$scope.selectedTags.length;i++) 
+					{
+						if (item.tags && item.tags.indexOf($scope.selectedTags[i]) > -1) { 
+							return item;
+						}
+					}
+					return;
+				}
+				return item;
+			}
 		});
 	}
 });
